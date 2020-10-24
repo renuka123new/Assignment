4bc96307c3594ed18e35d3fd2dc5d6148789db0e
renuka123new <renukamohorir97@gmail.com>
2020-10-23 18:33:22 -0700

    Added main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "add.h";
+int main(){
+	int num1, num2;
+	printf("Enter first number: \n");
+	scanf("%d",&num1);
+	printf("Enter the second number: \n");
+	scanf("%d",&num2);
+	return 0;
+}
